e4eb5b2a7a2f83a011f8d0186ddb42715f378858
Shawon301910 <shawonkumarsarkar.bd@gmail.com>
2024-02-18 23:22:32 +0600

    seats left and add selected seats number

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
+// select button --------------------------------------------------
+
 const buttons = document.getElementsByClassName("overlay");
 
+
 for(const button of buttons) {
-    button.addEventListener('click', function(event){
-        const selected = button.getAttribute('id');
-        overlay(selected);
-        console.log(selected)
+    button.addEventListener("click", function(event){
+
+        // get all button id -----------------------------
+
+        const selected = button.getAttribute("id");
+
+        // get seats left id -----------------------------
+
+        const seatsLeft = document.getElementById("seats-left").innerText;
+        const seatsLeftInt = parseInt(seatsLeft);
+
+        // get selected seat num id ----------------------
+
+        const selectSeatNum = document.getElementById("selected-seat-num").innerText;
+        const selectSeatNumInt = parseInt(selectSeatNum);
+        console.log(selectSeatNum)
+        
+        if(seatsLeftInt > 36) {
+
+            // selected ticket ---------------------------
+
+            overlay(selected);
+
+            // seats available ---------------------------
+
+            const available = seatsLeftInt - 1;
+            showElement("seats-left", available);
+
+            // selected seats number ---------------------
+
+            const selectedNum = selectSeatNumInt + 1;
+            showElement("selected-seat-num", selectedNum)
+        }
+        
     })
 }
 
--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,8 @@ function overlay(elementId) {
     change.classList.remove('text-second-color\/60', 'bg-gray-100/80')
     change.classList.add('extra')
 }
+
+function showElement(elementId, value) {
+    const currentElement = document.getElementById(elementId);
+    currentElement.innerText = value;
+}
